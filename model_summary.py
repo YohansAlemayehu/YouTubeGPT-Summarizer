d0ffef50a800fccd272bfe680a95001294f13391
Yohans Brhane <37708712+YohansAlemayehu@users.noreply.github.com>
2023-11-02 21:06:05 +0100

    Update model_summary.py

--- model_summary.py
+++ model_summary.py
@@ -125,7 +125,7 @@ def generate_answer(api_key: str, url: str, question: str) -> str:
         documents = loader.load()
 
     texts = text_splitter.split_documents(documents)
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embeddings = OpenAIEmbeddings()
     db = Chroma.from_documents(texts, embeddings)
     retriever = db.as_retriever()
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
